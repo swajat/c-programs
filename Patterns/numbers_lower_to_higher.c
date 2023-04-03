e18a3ed92013106f1c10fb8813e04f8d31397a2d
gyana ranjan <129391187+swajat@users.noreply.github.com>
2023-04-03 16:27:21 +0530

    Create numbers_lower_to_higher.c

---
+++ Patterns/numbers_lower_to_higher.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char i,j,rows;
+    printf("enter the num of rows");
+    scanf("%d",&rows);
+    
+    for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=i;j++)
+    {
+        printf("%d", j );
+    }
+    printf("\n");
+    
+    }
+
+    return 0;
+}
+
+*/
+  input (5)
+  1
+  12
+  123
+  1234
+  12345
+  /*
