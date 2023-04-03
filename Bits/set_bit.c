761dc79e4e42dde6be8d1f60c07f2931b41ecb25
gyana ranjan <129391187+swajat@users.noreply.github.com>
2023-04-03 14:48:19 +0530

    Update set_bit.c

--- Bits/set_bit.c
+++ Bits/set_bit.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+int num,pos;
+printf("enter the number\n");
+scanf("%d",&num);
+printf("enter the position \n");
+scanf("%d",&pos);
+printf("before number : %d\n",num);
+num=num|1<<pos;
+printf("after number:%d\n",num);
+
+
+    return 0;
+    }
+    /*
+    enter the number 10
+    enter the position  0
+    number will be 11
+    */
