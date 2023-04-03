0aa9c4503646cf0006e61aa2fbab5426ce37f35d
gyana ranjan <129391187+swajat@users.noreply.github.com>
2023-04-03 16:09:13 +0530

    Update pattern.c

--- Patterns/pattern.c
+++ Patterns/pattern.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+int main()
+{
+int j,i;
+for(j=1;j<=6;j++)
+{
+    for(i=1;i<=5;i++)
+    {
+        printf("A");
+    }
+    printf("\n");
+}
+    return 0;
+}
+
+*/
+  AAAAA
+  AAAAA
+  AAAAA
+  AAAAA
+  AAAAA
+  AAAAA
+  /*
