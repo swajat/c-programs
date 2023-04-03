043a3e5679a413d6fdd400d74663cc44fc196a5f
gyana ranjan <129391187+swajat@users.noreply.github.com>
2023-04-03 16:22:54 +0530

    Create given_input.c

---
+++ Patterns/given_input.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int i,j,rows;
+printf("enter the no of rows");
+scanf("%d",&rows);
+for(i=rows;i>=1;i--)
+{
+    for(j=1;j<=i;j++)
+{
+    printf("*");
+    
+}   
+printf("\n");
+
+}
+return 0;
+}
+*/
+  input (5)
+  *****
+  ****
+  ***
+  **
+  *
+  /*
