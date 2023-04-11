89cf971b3e80bee1073defa1effe8a970c692919
gyana ranjan <129391187+swajat@users.noreply.github.com>
2023-04-11 15:34:03 +0530

    Create toggle_bit.c

---
+++ Bits/toggle_bit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, n, newNum;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+  
+    printf("Enter nth bit to toggle (0-31): ");
+    scanf("%d", &n);
+
+    newNum = num ^ (1 << n);
+
+    printf("Bit toggled successfully.\n\n");
+    printf("Number before toggling %d bit: %d (in decimal)\n", n, num);
+    printf("Number after toggling %d bit: %d (in decimal)\n", n, newNum);
+
+    return 0;
+}
+/*
+
+enter any nmber  10
+ enter nth bit to toggle  2
+ 
+ after toggle  14
+
+*/
